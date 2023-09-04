5c4beeced7bdcfd3b8af272f43e342e532f8c182
comradeintense <in10se_SL@yahoo.com>
2023-09-05 02:00:21 +0300

    updated stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
 	theme: {
 		screens: {
 			xs: '580px',
@@ -12,18 +13,15 @@ module.exports = {
 		container: {
 			center: true,
 			padding: {
-				DEFAULT: '1.25rem',
+				DEFAULT: '1.5rem',
 				xs: '1.5rem',
 				sm: '2rem',
 				md: '3rem',
-				lg: '3rem',
+				lg: '3.625rem',
 				xl: '5rem',
 			},
 		},
 		extend: {
-			fontFamily: {
-				jakarta: ['Montserrat', 'sans-serif'],
-			},
 			colors: {
 				'main-bg': '#F8F8F8',
 				'dark-text': '#1E1E1E',
